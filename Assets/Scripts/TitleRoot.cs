c2e628748deb9d307262d3d3f45bdec11a2a6814
makimaki510151 <rsv95076@gmail.com>
2024-09-05 13:46:19 +0900

    add　タイトルの作成

--- Assets/Scripts/TitleRoot.cs
+++ Assets/Scripts/TitleRoot.cs
@@ -1,19 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleRoot : RootParent
 {
+    [SerializeField]
+    private GameObject settingObject = null;
+    [SerializeField]
+    private List<Button> mainButtons = new();
+    [SerializeField]
+    private EventSystem eventSystem = null;
+    [SerializeField]
+    private GameObject settingFirstObject = null;
+    [SerializeField]
+    private GameObject mainFirstObject = null;
+
+    private GameObject selectEndButtonObject = null;
+
     public static TitleRoot Instance;
+    private AsyncOperation asyncLoad;
+    private bool isCoroutines = false;
+
     public override void Awake() 
     {  
         base.Awake();
         Instance = this;
     }
 
-    public void SettingClose()
+    private void Update()
     {
-
+        if(selectEndButtonObject != eventSystem.currentSelectedGameObject && eventSystem.currentSelectedGameObject != null)
+        {
+            selectEndButtonObject = eventSystem.currentSelectedGameObject;
+        }
+        
+        if(eventSystem.currentSelectedGameObject == null) 
+        {
+            EventSystem.current.SetSelectedGameObject(selectEndButtonObject);
+        }
     }
 
+    public void ButtonNext()
+    {
+        if (isCoroutines) return;
+        isCoroutines = true;
+        dataScriptableObject.playType = DataScriptableObject.PlayType.Two;
+        StartCoroutine(LoadYourAsyncScene("MainGame"));
+    }
+
+    public void ButtonGamedEnd()
+    {
+        Application.Quit();
+    }
+    public void ButtonSetting()
+    {
+        settingObject.SetActive(true);
+        for(int i = 0; i < mainButtons.Count; i++)
+        {
+            mainButtons[i].interactable = false;
+        }
+        EventSystem.current.SetSelectedGameObject(settingFirstObject);
+    }
+
+    public void SettingClose()
+    {
+        for (int i = 0; i < mainButtons.Count; i++)
+        {
+            mainButtons[i].interactable = true;
+        }
+        EventSystem.current.SetSelectedGameObject(mainFirstObject);
+    }
+    IEnumerator LoadYourAsyncScene(string name)
+    {
+        asyncLoad = SceneManager.LoadSceneAsync(name);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
 }
