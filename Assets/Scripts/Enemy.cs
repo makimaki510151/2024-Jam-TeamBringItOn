8b8c0ef8919d9507c80242e8c20cd78fe611e73d
makimaki510151 <rsv95076@gmail.com>
2024-09-05 11:47:14 +0900

    add 蟹追加

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,14 +5,64 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private GameObject myUI = null;
 
+    enum EnemyAiType
+    {
+        Idle,
+        Crab
+    }
+    [SerializeField]
+    private EnemyAiType aiType = EnemyAiType.Idle;
+    [SerializeField]
+    private float crabMoveTime = 1;
+    private float crabMoveTimer = 0;
+    [SerializeField]
+    private float crabSpeed = 5;
+    [SerializeField]
+    private int crabCoefficient = 1;
+
+
+
     private Vector3 tempVector3 = new(0,0,0);
     private GameObject tempObject = null;
 
+    private Rigidbody2D myRigidbody2D = null;
     private Transform myTransform = null;
+
+
 
     private void Start()
     {
         myTransform = transform;
+        switch (aiType)
+        {
+            case EnemyAiType.Idle:
+                break;
+            case EnemyAiType.Crab:
+                crabMoveTimer = crabMoveTime;
+                myRigidbody2D = GetComponent<Rigidbody2D>();
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        switch (aiType)
+        {
+            case EnemyAiType.Idle:
+                break;
+            case EnemyAiType.Crab:
+                if(crabMoveTimer >0)
+                {
+                    crabMoveTimer -= Time.deltaTime;
+                    myRigidbody2D.velocity = myTransform.right * crabCoefficient * crabSpeed;
+                    if (crabMoveTimer <= 0)
+                    {
+                        crabMoveTimer = crabMoveTime;
+                        crabCoefficient *= -1;
+                    }
+                }
+                break;
+        }
     }
 
     public void StockMove(Player.PlayCharacter playCharacter)
