f0874f0817be1b6d19dd7ee634b0eabe8b3719d5
kousei takamatsu <takamatsu7576@gmail.com>
2024-09-05 15:18:02 +0900

    [add] 背景のスクロール機能を追加

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class BackGround : MonoBehaviour
+{
+    [SerializeField, Tooltip("プレイヤー")]
+    private Transform playerTransform = null;
+
+    [SerializeField, Tooltip("最初の位置")]
+    private float startPoint = 0;
+
+    [SerializeField, Tooltip("最後の位置")]
+    private float goalPoint = 0;
+
+    [SerializeField, Tooltip("マップの長さ")]
+    private float mapRange = 0;
+
+    private float height = 0;
+    private Vector2 start;
+    private Vector2 goal;
+
+    Transform myTransform;
+
+    void Start()
+    {
+        myTransform = transform;
+        height = myTransform.position.y;
+    }
+
+    void Update()
+    {
+        start = new Vector2(startPoint + playerTransform.position.x, height);
+        goal = new Vector2(goalPoint + playerTransform.position.x, height);
+        myTransform.position = Vector2.Lerp(start, goal, playerTransform.position.x / mapRange);
+    }
+}
