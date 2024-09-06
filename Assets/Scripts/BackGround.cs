60b89ae1d501ab7d44b2c702851fca5913009116
kousei takamatsu <takamatsu7576@gmail.com>
2024-09-06 12:36:55 +0900

    [add] 火の背景を変更

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -14,6 +14,20 @@ public class BackGround : MonoBehaviour
     [SerializeField, Tooltip("マップの長さ")]
     private float mapRange = 0;
 
+    [Header("無限ループ")]
+
+    [SerializeField, Tooltip("無限ループ化")]
+    private bool isLoop = false;
+
+    [SerializeField]
+    private Transform[] Sprites = null;
+
+    [SerializeField, Tooltip("カメラ")]
+    private Transform playerCamera = null;
+
+    [SerializeField, Tooltip("背景の幅")]
+    private float backGroundWidth = 23.13f;
+
     private float height = 0;
     private Vector2 start;
     private Vector2 goal;
@@ -28,8 +42,22 @@ public class BackGround : MonoBehaviour
 
     void Update()
     {
-        start = new Vector2(startPoint + playerTransform.position.x, height);
-        goal = new Vector2(goalPoint + playerTransform.position.x, height);
-        myTransform.position = Vector2.Lerp(start, goal, playerTransform.position.x / mapRange);
+        if (!isLoop)
+        {
+            start = new Vector2(startPoint + playerTransform.position.x, height);
+            goal = new Vector2(goalPoint + playerTransform.position.x, height);
+            myTransform.position = Vector2.Lerp(start, goal, playerTransform.position.x / mapRange);
+        }
+        else
+        {
+            var cameraGridX = Mathf.FloorToInt(playerCamera.position.x / backGroundWidth);
+
+            for (int index = 0; index < Sprites.Length; index++)
+            {
+                var position = Sprites[index].position;
+                position.x = (index - 1 + cameraGridX) * backGroundWidth;
+                Sprites[index].position = position;
+            }
+        }
     }
 }
