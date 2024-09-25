940583815c276bd1743901d12b68df5d80001fad
kousei takamatsu <takamatsu7576@gmail.com>
2024-09-26 02:55:29 +0900

    [add] コイン取得音を追加　水の精のゴールUIを差し替え

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -7,11 +7,18 @@ public class Coin : MonoBehaviour
     [SerializeField]
     private int coinPower = 1;
 
+    [SerializeField]
+    private int pickCoinSEVol = 1;
+    [SerializeField]
+    private AudioClip pickCoinSE = null;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<Player>().BuffUp(coinPower);
+            AudioControl.Instance.SetSEVol(pickCoinSEVol);
+            AudioControl.Instance.PlaySE(pickCoinSE);
             Destroy(gameObject);
         }
     }
